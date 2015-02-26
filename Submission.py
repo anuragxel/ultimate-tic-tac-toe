51c1eed2cfd71de070c4dd263f020b9a08d0dccc
Anurag Ghosh <anurag.ghosh@research.iiit.ac.in>
2015-02-26 17:02:26 +0530

    Cleanup. Not Matlab here, bitch.

--- Submission.py
+++ Submission.py
@@ -32,7 +32,7 @@ class Player(object):
 		has_lost = False
 		has_completed = False
 		first_win=0		#0=none 1=me 2=other 
-		x,y = get_block_coords(block_number)
+		x,y = self.get_block_coords(block_number)
 
 		our_symbol = self.player_symbol
 		other_symbol = self.opponent_symbol
@@ -44,68 +44,68 @@ class Player(object):
 
 		if current_block[x][y] == our_symbol and current_block[x + 1][y] == our_symbol and current_block[x + 2][y] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x][y + 1] == our_symbol and current_block[x + 1][y + 1] == our_symbol and current_block[x + 2][y + 1] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x][y + 2] == our_symbol and current_block[x + 1][y + 2] == our_symbol and current_block[x + 2][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x][y] == our_symbol and current_block[x][y + 1] == our_symbol and current_block[x][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x + 1][y] == our_symbol and current_block[x + 1][y + 1] == our_symbol and current_block[x + 1][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x + 2][y] == our_symbol and current_block[x + 2][y + 1] == our_symbol and current_block[x + 2][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x][y] == our_symbol and current_block[x + 1][y + 1] == our_symbol and current_block[x + 2][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		elif current_block[x + 2][y] == our_symbol and current_block[x + 1][y + 1] == our_symbol and current_block[x][y + 2] == our_symbol:
 			has_won = True
-			if first_win==0
+			if first_win==0:
 				first_win=1
 		
 		if current_block[x][y] == other_symbol and current_block[x + 1][y] == other_symbol and current_block[x + 2][y] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x][y + 1] == other_symbol and current_block[x + 1][y + 1] == other_symbol and current_block[x + 2][y + 1] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x][y + 2] == other_symbol and current_block[x + 1][y + 2] == other_symbol and current_block[x + 2][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x][y] == other_symbol and current_block[x][y + 1] == other_symbol and current_block[x][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x + 1][y] == other_symbol and current_block[x + 1][y + 1] == other_symbol and current_block[x + 1][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x + 2][y] == other_symbol and current_block[x + 2][y + 1] == other_symbol and current_block[x + 2][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x][y] == other_symbol and current_block[x + 1][y + 1] == other_symbol and current_block[x + 2][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		elif current_block[x + 2][y] == other_symbol and current_block[x + 1][y + 1] == other_symbol and current_block[x][y + 2] == other_symbol:
 			has_lost = True
-			if first_win==0
+			if first_win==0:
 				first_win=-1
 		return (has_completed,first_win)
 
@@ -135,25 +135,41 @@ class Player(object):
 		return blocks_allowed
 
 	def get_empty_out_of(self, permitted_blocks):
-	gameb = self.current_board
-	blal = permitted_blocks
-	cells = []
-	for idb in blal:
-		id1 = idb/3
-		id2 = idb%3
-		for i in range(id1*3,id1*3+3):
-			for j in range(id2*3,id2*3+3):
-				if gameb[i][j] == '-':
-					cells.append((i,j))
-	if cells == []:
-		for i in range(9):
-			for j in range(9):
-				if gameb[i][j] == '-':
-					cells.append((i,j))	
-	return cells
+		gameb = self.actual_board
+		blal = permitted_blocks
+		cells = []
+		for idb in blal:
+			id1 = idb/3
+			id2 = idb%3
+			for i in range(id1*3,id1*3+3):
+				for j in range(id2*3,id2*3+3):
+					if gameb[i][j] == '-':
+						cells.append((i,j))
+		if cells == []:
+			for i in range(9):
+				for j in range(9):
+					if gameb[i][j] == '-':
+						cells.append((i,j))	
+		return cells
 
 	def get_baseline_allowed_moves(self,current_board,moves): 		#permitted moves(gand bachao)
 		pass
+
+	def game_completed(self,current_board,our_symbol):
+		q = [0 for x in xrange(0,9)]
+		w = [0 for x in xrange(0,9)]
+		j=0
+		for i in xrange(0,9):
+			q[i],w[i]=self.get_status_of_block(i,current_board,our_symbol)
+		for i in xrange(0,9):
+			if q[i]==True or w[i]!=0:
+				j += 1
+		if w[1]+w[2]+w[0]==3 or w[3]+w[4]+w[5]==3 or w[6]+w[7]+w[8]==3 or w[0]+w[3]+w[6]==3 or w[1]+w[4]+w[7]==3 or w[2]+w[5]+w[8]==3 or w[0]+w[5]+w[8]==3 or w[2]+w[5]+w[7]==3:
+			return (j,10)
+		elif w[1]+w[2]+w[0]==-3 or w[3]+w[4]+w[5]==-3 or w[6]+w[7]+w[8]==-3 or w[0]+w[3]+w[6]==-3 or w[1]+w[4]+w[7]==-3 or w[2]+w[5]+w[8]==-3 or w[0]+w[5]+w[8]==-3 or w[2]+w[5]+w[7]==-3:
+			return (j,-10)
+		else:
+			return (j,0)
 
 	def return_random_move(self,possible_moves):
 		return random.choice(possible_moves)
@@ -171,68 +187,48 @@ class Player(object):
 		self.actual_board = current_board[:]
 		self.status_board = board_stat[:]
 
+	def min_max_with_alpha_beta_pruning(self,opponent_move,our_symbol):
+		blocks_allowed = self.get_permitted_blocks(opponent_move)
+		cells = self.get_empty_out_of(blocks_allowed)
+		game_status,game_score = self.game_completed(self.actual_board,'x')
+		if game_status == 9: # Terminal Condition
+			return ((-1,-1),game_score) #-10or0or10 and move that can't be made
+		else:
+			alpha=beta=utility=((-1,-1), float("nan"))
+			if our_symbol == 'x':
+				for cell in cells:
+					x,y = cell
+					self.actual_board[x][y] = 'x'
+					child = self.min_max_with_alpha_beta_pruning(cell,'o')
+					self.actual_board[x][y] = '-'
+					if utility[1] == float("nan") or utility[1] < child[1]:
+						utility = (cell,child[1])
+					if alpha[1] == float("nan") or utility[1] > alpha[1]:
+						alpha = utility
+					if beta[1] != float("nan") and alpha[1] > beta[1]:
+						break
+				return alpha
+			elif our_symbol == 'o':
+				for cell in cells:
+					x,y = cell
+					self.actual_board[x][y] = 'o'
+					child = self.min_max_with_alpha_beta_pruning(cell,'x')
+					self.actual_board[x][y] = '-'
+					if utility[1] == float("nan") or utility[1] > child[1]:
+						utility = (cell,child[1])
+					if alpha[1] == float("nan") or utility[1] < beta[1]:
+						beta = utility
+					if beta[1] != float("nan") and alpha[1] > beta[1]:
+						break
+				return beta
+
 	def move(self,current_board,board_stat,opponent_move,our_symbol):
 		'''
 		Parameters - opponent_move - <(a,b)> previous move by opponent; board_stat - <[]> info of blocks won/lost; 
 					current_board - <[]> current board situation; our_symbol
 		Return Value - move- <(row,column)> 
 		'''
-	#	bind_symbol(our_symbol)
-	#	copy_current_board_elems(current_board,board_stat)
-	#	mvp = raw_input()
-	#	mvp = mvp.split()
-	#	self.number_of_moves += 1
-	#	return current_move
-		blocks_allowed=get_permitted_blocks(opponent_move)
-		cells=get_empty_out_of(blocks_allowed)
-		game_status,game_score=game_completed(current_board,'x')
-		if game_status==9:
-			return game_score #-10||0||10
-		else:
-			alpha=beta=utiity=NULL
-			if our_symbol=='x':
-				for cell in cells:
-					current_board[cell]='x'
-					a=move(self,current_board,board_stat,cell,'o')
-					current_board[cell]='-'
-					if utiity== NULL || utiity<a:
-						utiity=a
-					if utiity > alpha || alpha == NULL:
-						alpha=utiity
-					if alpha> beta && beta != NULL:
-						break
-				return alpha
-			elif our_symbol == 'o':
-				for cell in cells:
-					current_board[cell]='o'
-					a=move(self,current_board,board_stat,cell,'x')
-					current_board[cell]='-'
-					if utiity== NULL || utiity>a:
-						utiity=a
-					if utiity < beta || alpha == NULL:
-						beta=utiity
-					if alpha> beta && beta != NULL:
-						break
-				return beta 
-
-
-# get_empty_out_of(gameboard, permitted_blocks) returns possible_moves
-
-
-
-
-def game_completed(self,current_board,our_symbol):
-	q[]=w[]=0
-	j=0
-	for i in xrange(0,8):
-		q[i],w[i]=get_status_of_block(i,current_block,our_symbol)
-	for i in xrange(0,8):
-		if q[i]==True || w[i]!=0:
-			j++
-	if w[1]+w[2]+w[0]==3 || w[3]+w[4]+w[5]==3 || w[6]+w[7]+w[8]==3 || w[0]+w[3]+w[6]==3 || w[1]+w[4]+w[7]==3 || w[2]+w[5]+w[8]==3 ||w[0]+w[5]+w[8]==3 || w[2]+w[5]+w[7]==3:
-		return (j,10)
-	elif w[1]+w[2]+w[0]==-3 || w[3]+w[4]+w[5]==-3 || w[6]+w[7]+w[8]==-3 || w[0]+w[3]+w[6]==-3 || w[1]+w[4]+w[7]==-3 || w[2]+w[5]+w[8]==-3 ||w[0]+w[5]+w[8]==-3 || w[2]+w[5]+w[7]==-3:
-		return (j,-10)
-	else:
-		return (j,0)
-
+		self.bind_symbol(our_symbol)
+		self.copy_current_board_elems(current_board,board_stat)
+		move,value = self.min_max_with_alpha_beta_pruning(opponent_move,self.player_symbol)
+		return move
